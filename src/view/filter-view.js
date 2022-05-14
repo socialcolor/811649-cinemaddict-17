f03d0644828a75acb8c26bb26129550f71f0ae5f
socialcolor <64646814+socialcolor@users.noreply.github.com>
2022-05-14 18:04:46 +0300

    Шаблонизировал фильтры

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,16 +1,27 @@
 import AbstractView from '../framework/view/abstract-view';
 
-const createFilterTemplate = () => (
-  `<nav class="main-navigation">
+const createFilterTemplate = (watchlist, favorite) => {
+  const watchlistLenght = watchlist.length;
+  const favoriteLenght = favorite.length;
+  return `<nav class="main-navigation">
       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlistLenght}</span></a>
       <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-  </nav>`
-);
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favoriteLenght}</span></a>
+  </nav>`;
+};
 
 export default class FilterView extends AbstractView {
+  #watchlist = null;
+  #favorite = null;
+
+  constructor (watchlist, favorite) {
+    super();
+    this.#watchlist = watchlist;
+    this.#favorite = favorite;
+  }
+
   get template() {
-    return createFilterTemplate();
+    return createFilterTemplate(this.#watchlist, this.#favorite);
   }
 }
