e8f363091e9f53c7e01f9893da7f094f0b72325e
socialcolor <64646814+socialcolor@users.noreply.github.com>
2022-06-21 19:16:45 +0300

    Fix Б18

--- src/const.js
+++ src/const.js
@@ -30,55 +30,6 @@ const SortType = {
   RATING: 'rating',
 };
 
-const DIRTCTORS = [
-  'Guillermo del Toro',
-  'David Cronenberg',
-  'Woody Allen',
-  'Tim Burton',
-  'Akira Kurosawa',
-  'Alfred Hitchcock',
-  'Stanley Kubrick',
-];
-
-const GENRES = [
-  'Comedy',
-  'Drama',
-  'Fantasy',
-  'Horror',
-];
-
-const COUNTRIES = [
-  'USA',
-  'England',
-  'France',
-  'Italy',
-];
-
-const POSTERS = [
-  'made-for-each-other.png',
-  'popeye-meets-sinbad.png',
-  'sagebrush-trail.jpg',
-  'santa-claus-conquers-the-martians.jpg',
-  'the-dance-of-life.jpg',
-  'the-great-flamarion.jpg',
-  'the-man-with-the-golden-arm.jpg',
-];
-
-const AUTHORS_COMMENT = [
-  'Tim Macoveev',
-  'John Doe',
-  'Ilya O\'Reilly',
-  'Igor Petrov',
-];
-
-const COMMENTS = [
-  'Interesting setting and a good cast',
-  'Booooooooooring',
-  'Very very old. Meh',
-  'Almost two hours? Seriously?',
-  'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
-];
-
 const EMOTIONS = [
   'smile',
   'sleeping',
@@ -109,12 +60,6 @@ export {
   COMMENTS_LENGTH,
   TOP_RATED_FILMS,
   MOST_COMMENTS_FILMS,
-  DIRTCTORS,
-  GENRES,
-  COUNTRIES,
-  POSTERS,
-  AUTHORS_COMMENT,
-  COMMENTS,
   EMOTIONS,
   FILM_COUNT_PER_STEP,
   FiltersType,
